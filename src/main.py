b1b47c7462bdc79f7a5931d67093f5ff7c7435d3
Alpha2749 <Alpha27492@gmail.com>
2025-07-12 23:48:03 +0930

    added textnode to htmlnode converter

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
+from htmlnode import HTMLNode, LeafNode, ParentNode
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(tag=None,value=text_node.text)
+        case TextType.BOLD:
+            return LeafNode(tag="b",value=text_node.text)
+        case TextType.ITALIC:
+            return LeafNode(tag="i",value=text_node.text)
+        case TextType.CODE:
+            return LeafNode(tag="code",value=text_node.text)
+        case TextType.LINK:
+            return LeafNode(tag="a",value=text_node.text, props={"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode(tag="img",value="", props={"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("Invalid node type")
+        
+
 
 def main():
     test_obj = TextNode("This is some anchor text", "link", "https://www.boot.dev")
     print(test_obj)
 
 main()
-
-    
