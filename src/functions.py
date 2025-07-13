da32e744c361d607e40a42f235ce24b8f2452843
Alpha2749 <Alpha27492@gmail.com>
2025-07-13 13:43:15 +0930

    modified block_to_block logic

--- src/functions.py
+++ src/functions.py
@@ -112,26 +112,15 @@ def markdown_to_blocks(markdown):
 def block_to_block_type(block):
     lines = block.split("\n")
 
-    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+    if re.match(r"^#{1,6} ", lines[0]):
         return BlockType.HEADING
     if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
         return BlockType.CODE
-    if block.startswith(">"):
-        for line in lines:
-            if not line.startswith(">"):
-                return BlockType.PARAGRAPH
+    if all(line.startswith(">") for line in lines):
         return BlockType.QUOTE
-    if block.startswith("- "):
-        for line in lines:
-            if not line.startswith("- "):
-                return BlockType.PARAGRAPH
+    if all(line.startswith("- ") for line in lines):
         return BlockType.UNORDERED_LIST
-    if block.startswith("1. "):
-        i = 1
-        for line in lines:
-            if not line.startswith(f"{i}. "):
-                return BlockType.PARAGRAPH
-            i += 1
+    if all(re.match(r"^\d+\. ", line) for line in lines):
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
 
