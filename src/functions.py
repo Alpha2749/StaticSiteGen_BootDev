0d218f773862bc6051c37907698259e749db72f1
Alpha2749 <Alpha27492@gmail.com>
2025-07-13 17:50:18 +0930

    add docs

--- src/functions.py
+++ src/functions.py
@@ -4,6 +4,7 @@ from enum import Enum
 from htmlnode import ParentNode
 import os
 import shutil
+from pathlib import Path
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -245,30 +246,38 @@ def extract_title(markdown):
             return line[2:].strip()
     raise Exception("No h1 header found in markdown")
 
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-
-    with open(from_path, "r") as f:
-        markdown = f.read()
-    with open(template_path, "r") as f:
-        template = f.read()
-
-    title = extract_title(markdown)
-    content = markdown_to_html_node(markdown).to_html()
-
-    html = template.replace("{{ Title }}", title).replace("{{ Content }}", content)
-
-    with open(dest_path, "w") as f:
-        f.write(html)
-
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    if not os.path.exists(dest_dir_path):
-        os.mkdir(dest_dir_path)
-
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
-        dest_path = os.path.join(dest_dir_path, filename.replace(".md", ".html"))
-        if os.path.isfile(from_path) and from_path.endswith(".md"):
-            generate_page(from_path, template_path, dest_path)
-        elif os.path.isdir(from_path):
-            generate_pages_recursive(from_path, template_path, dest_path)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+
+
+def generate_page(from_path, template_path, dest_path, basepath):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
